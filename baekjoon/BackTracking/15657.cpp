52a6519fbb17e1c10676442fac702f8b7ed71a9e
kimwiseong <kwws919@gmail.com>
2022-05-18 13:26:43 +0900

    add 15657

---
+++ baekjoon/BackTracking/15657.cpp
@@ -0,0 +1,28 @@
+/** dfs, backtracking, N과 M(8) */
+#include<bits/stdc++.h>
+using namespace std;
+int N, M, num[10], arr[10];
+
+void dfs(int x, int pre) {
+    if(x == M) {
+        for (int i = 0; i < M; i++)
+            cout << num[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = pre; i < N; i++) {
+        num[x] = arr[i];
+        dfs(x+1, i);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) 
+        cin >> arr[i];
+    sort(arr, arr+N);
+    dfs(0,0);
+}

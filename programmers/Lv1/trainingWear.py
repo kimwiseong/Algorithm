3fe95cf37b7c29eb087b7e3e8b0e160eba27621f
kimwiseong <kwws919@gmail.com>
2022-07-19 00:23:06 +0900

    add solved

---
+++ programmers/Lv1/trainingWear.py
@@ -0,0 +1,9 @@
+def solution(n, lost, reserve):
+    ls = set(lost) - set(reserve)
+    res = set(reserve) - set(lost)
+    for i in res:
+        if i-1 in ls: ls.remove(i-1)
+        elif i+1 in ls: ls.remove(i+1)
+    return n - len(ls)
+
+print(solution(5, [2, 4], [1, 3, 5]))

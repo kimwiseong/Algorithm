17d6c6ae125a6db005cf9b42d017d0d978a17587
kimwiseong <kwws919@gmail.com>
2022-08-04 04:16:22 +0900

    add solved

---
+++ programmers/Lv2/nextBigNum.py
@@ -0,0 +1,10 @@
+def solution(n):
+    count = bin(n).count('1')
+
+    while True:
+        n += 1
+        if bin(n).count('1') == count : 
+            break;
+    return n
+
+print(solution(78))

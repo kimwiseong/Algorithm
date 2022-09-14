0d3c0e337c75a754ed4c948dd68065b7f561af77
kimwiseong <kwws919@gmail.com>
2022-09-14 14:32:02 +0900

    add solved

---
+++ baekjoon/step/11478.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    string s;
+    cin >> s;
+
+    unordered_set<string> result;
+    int n = s.size();
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++) 
+            result.insert(s.substr(i, j));
+
+    cout << result.size() << '\n';
+}

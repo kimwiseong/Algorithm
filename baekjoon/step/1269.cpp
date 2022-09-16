d6740c3d07fdd9504dfb30b58581a0c8661685a6
kimwiseong <kwws919@gmail.com>
2022-09-16 12:09:11 +0900

    add solved

---
+++ baekjoon/step/1269.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n, m, num;
+    vector<int> A, B, result;
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        A.push_back(num);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> num;
+        B.push_back(num);
+    }
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    set_symmetric_difference(A.begin(), A.end(), B.begin(), B.end(), back_inserter(result));
+    cout << result.size() << '\n';
+}

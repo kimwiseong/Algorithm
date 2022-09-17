d664e4fb930f7672c21abc83ef9668d35b6ef602
kimwiseong <kwws919@gmail.com>
2022-09-17 11:47:50 +0900

    add solved

---
+++ baekjoon/step/17298.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    int a[1000000], nge[1000000];
+    stack<int> s;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+
+    for (int i = n-1; i >= 0; i--) {
+        while(!s.empty() && s.top() <= a[i]) s.pop();
+        
+        if(s.empty()) nge[i] = -1;
+        else nge[i] = s.top();
+        s.push(a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << nge[i] << ' ';
+    cout << '\n';
+}

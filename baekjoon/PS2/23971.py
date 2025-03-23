a18023b6701cc864c4c1d23706802c4940c5d0b9
kimwiseong <kwws919@gmail.com>
2025-03-23 21:35:01 +0900

    add 23971, math

--- baekjoon/PS2/23971.py
+++ baekjoon/PS2/23971.py
@@ -1,4 +1,4 @@
 import math
 
-h, w, n, m = map(int, input().split())
-print(math.ceil(h/(n+1)) * math.ceil(w/(n+1)))
+h, w, n, m = map(int, input().split(' '))
+print(math.ceil(h/(n+1)) * math.ceil(w/(m+1)))
